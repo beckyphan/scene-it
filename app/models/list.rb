d98b355d7d3235ecebc35f4a1b5b1f5789de9d4b
Becky Phan <beckyphan@live.com>
2019-11-10 21:45:36 -0500

    updated list scope

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
   belongs_to :user
   has_many :movies
-  scope :seen, { where (watched: true)}
-  scope :to_see, {where (watched: false)}
+  scope :seen, -> { where(watched: true)}
+  scope :to_see, -> {where(watched: false)}
+
+  #<List id: nil, user_id: nil, movie_id: nil, watched: nil, created_at: nil, updated_at: nil> 
 end
