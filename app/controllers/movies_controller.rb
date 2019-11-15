c679cf5c3ba03f38b2b0a6828b754a4ca0974c0b
Becky Phan <beckyphan@live.com>
2019-11-14 21:18:31 -0500

    movie#create form on movies#index view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,19 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path
+    else
+      render "index"
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :year, :category, :image_url)
+  end
 end
