4e2ddcbfd18df4f8fad98f37f8ca2a63984dbca9
Becky Phan <beckyphan@live.com>
2019-11-15 21:25:33 -0500

    updated movie controller show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,19 +21,24 @@ class MoviesController < ApplicationController
     @movie = Movie.find_by_id(params[:id])
 
     if @movie.ratings.size > 0
-      @avg_rating = @movie.average(:rating)
+      @avg_rating = @movie.ratings.where(recommend: true).size - @movie.ratings.where(recommend: false).size
     else
       @avg_rating = 0
     end
 
-    @list = List.new
+    @list = List.where("user_id = ? AND movie_id = ?", current_user.id, params[:id]).first
+
+    if !@list.nil?
+    else
+      @list = List.new
+    end
 
     @seen = false
 
     if current_user.lists.include?(movie_id: @movie.id)
       @seen = true
-      binding.pry
-      @rating = Rating.where("user_id = ? AND movie_id = ?", current_user.id, params[:id])
+
+      @rating = Rating.where("user_id = ? AND movie_id = ?", current_user.id, params[:id]).
       if @user_rating.recommend
         @user_rated = "user_recommended"
       else
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,27 +1,16 @@
 module MoviesHelper
 
-  def thumbs_rating(rating)
-    if rating == 0 || rating == nil
+  def thumbs_rating(avg_rating)
+    if avg_rating == nil
       html = "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Icon-round-Question_mark.svg/1024px-Icon-round-Question_mark.svg.png'>"
       html.html_safe
-    elsif rating > 0.5
-      html = "<img src='https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjNm_C3puvlAhVNSN8KHR-yBjEQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fthumbs-up_25297&psig=AOvVaw2LJ98o02wytNDcOnjU2ZSr&ust=1573875811649241'>"
+    elsif avg_rating > 0
+      html = "<img src='https://image.flaticon.com/icons/svg/25/25297.svg'>"
       html.html_safe
     else
       html = "<img src='https://image.flaticon.com/icons/png/512/25/25395.png'>"
       html.html_safe
     end
   end
-
-  def thumbs_up_button
-    html = "'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjNm_C3puvlAhVNSN8KHR-yBjEQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fthumbs-up_25297&psig=AOvVaw2LJ98o02wytNDcOnjU2ZSr&ust=1573875811649241'"
-    html.html_safe
-  end
-
-  def thumbs_down_button
-    html = "'https://image.flaticon.com/icons/png/512/25/25395.png'"
-    html.html_safe
-  end
-
 
 end
