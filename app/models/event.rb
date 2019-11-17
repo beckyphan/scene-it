45fb9da24342c577e4246ad027c2b186c77dfcad
Becky Phan <beckyphan@live.com>
2019-11-16 22:40:14 -0500

    corrected event model class: name format

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
-  belongs_to :host, class_name: :user, foreign_key: :user_id
-  has_many :attendees, class_name: :user, foreign_key: :user_id
-  belongs_to :movie
+  belongs_to :host, class_name: "User"
+  has_many :attendees, class_name: "User"
+  has_one :movie
 
-  #<Event id: nil, title: nil, date: nil, time: nil, location: nil, host_id: nil, movie_id: nil, created_at: nil, updated_at: nil> 
+  #<Event id: nil, title: nil, date: nil, time: nil, location: nil, host_id: nil, movie_id: nil, created_at: nil, updated_at: nil>
 end
