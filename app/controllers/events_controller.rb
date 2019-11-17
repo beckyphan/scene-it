b23c5c7edb5d29cb95ac336073fe4ef013ad6d09
Becky Phan <beckyphan@live.com>
2019-11-17 11:59:58 -0500

    updated events controller to show events page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,12 @@
 class EventsController < ApplicationController
+
+  def show
+    @event = Event.find_by_id(params[:id])
+    @movie = Movie.find_by_id(@event.movie_id)
+    @host = User.find_by_id(@event.host_id)
+    @attendees = @event.attendees
+    @comments = Comment.where(event_id: params[:id])
+
+  end
+
 end
